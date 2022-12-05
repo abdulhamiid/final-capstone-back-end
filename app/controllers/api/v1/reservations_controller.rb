7ee20a7d6a4ca55512fba49b51304f14660d4c84
XsidX <developer.sid@outlook.com>
2022-12-05 17:24:42 +0300

    Fix user sign in  problem

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -19,7 +19,6 @@ class Api::V1::ReservationsController < ApiController
   def create
     @reservation = current_user.reservations.new(reservation_params)
     @fitness_activity = FitnessActivity.find(params[:fitness_activity_id])
-    @reservation.user_id = current_user.id
     @reservation.fitness_activity_id = @fitness_activity.id
 
     if @reservation.save
