21fa06bbfd58ec55fd261233900742c21ca9cada
Abdulhamid <adioabdulhamid@yahoo.com>
2022-12-07 12:25:18 +0100

    Modify user authorization rule

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,15 @@ class Ability
     if user.admin == true
       can :manage, :all
     else
-      can :read, Users, id: user.id
+      can :destroy, Reservation do |reservation|
+        reservation.user == user
+      end
+
+      can :update, Reservation do |reservation|
+        reservation.user == user
+      end
+      can :create, Reservation
+      can :read, User, id: user.id
       can :read, Reservation, user_id: user.id
       can :read, FitnessActivity
     end
