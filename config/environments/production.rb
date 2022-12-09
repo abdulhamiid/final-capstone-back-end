294cb502d6ea6ad230d19e9b5002a7b04b096346
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-09 12:42:10 +0500

    Add railway.app host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  routes.default_url_options[:host] = 'https://final-capstone-back-end-production.up.railway.app'
+  config.hosts << "https://final-capstone-back-end-production.up.railway.app"
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
